93a02507a56bf0c05aec3fd8eba51d170a137c49
jhs99126 <jhs99126@naver.com>
2024-11-26 16:05:07 +0900

    add ReadOnlyRepository

--- src/main/java/com/sportuniform/repository/SizeRepository.java
+++ src/main/java/com/sportuniform/repository/SizeRepository.java
@@ -1,10 +1,9 @@
 package com.sportuniform.repository;
 
 import com.sportuniform.domain.Size;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface SizeRepository extends JpaRepository<Size, Long> {
+public interface SizeRepository extends ReadOnlyRepository<Size, Long> {
     Optional<Size> findBySize(String size);
 }
